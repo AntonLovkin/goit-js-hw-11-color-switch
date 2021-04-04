2e01496adf3685ec37fd2f21f1cecd70f9e47a4c
AntonLovkin <anton28111987@gmail.com>
2021-04-04 20:38:15 +0300

    працює

--- index.js
+++ index.js
@@ -7,3 +7,39 @@ const colors = [
   '#795548',
 ];
 
+//--изменение цвета body рандомным цветом из массива---
+
+const changeBodyColor = function () {
+
+  if (startBtn.classList.contains("inabled")) {
+    return
+  }
+    
+  const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomNumber = randomIntegerFromInterval(0, colors.length - 1);
+const randomColor = colors[randomNumber];
+const bodyRef = document.querySelector('body');
+
+  bodyRef.style.backgroundColor = randomColor;
+ }
+
+//--привязка изменения цвета к кнопкам--
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(changeBodyColor, 1000);
+  console.log('active');
+  startBtn.setAttribute("disabled", true)
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log('pushed');
+  startBtn.removeAttribute("disabled")
+})
+
+
